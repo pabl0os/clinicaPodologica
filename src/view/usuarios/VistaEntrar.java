55b198eb312a63f5bcab276129fae6fe0749d030
Juan Pablo <pablinchus12@gmail.com>
2024-11-02 19:54:20 -0600

    se agrego color a todos los menus y a los items el color azul y un color blanco a las letras de dichos elementos

--- src/view/usuarios/VistaEntrar.java
+++ src/view/usuarios/VistaEntrar.java
@@ -20,7 +20,6 @@ import controller.WindowMain;
 
 public class VistaEntrar {
 
-  
   JPanel jp_panelprincipal = new JPanel();
 
   // LOGIN
@@ -29,7 +28,7 @@ public class VistaEntrar {
   public JPanel getJp_login() {
     if (jp_login == null) {
       jp_login = new JPanel();
-      
+
       // variables
       JLabel jl_titulo = new JLabel("Clinica Podologica Zane");
       JLabel jl_correo = new JLabel("Correo");
@@ -39,25 +38,24 @@ public class VistaEntrar {
       ImageIcon logo = new ImageIcon("src/image/Sin título.png");
       JLabel jl_imagen = new JLabel(logo);
       JButton jb_entrar = new JButton("Entrar");
-      
+
       // ATRIBUTOS
       jl_titulo.setFont(new Font("Arial", Font.BOLD, 24));
       jl_titulo.setBounds(250, 50, 400, 40);
       jl_correo.setBounds(250, 150, 50, 20);
       jl_pass.setBounds(230, 200, 70, 20);
       jl_imagen.setBounds(500, -150, 400, 400);
-      
+
       jp_login.setBackground(new Color(173, 232, 244));
       jp_login.setLayout(null);
       jp_panelprincipal.setBackground(new Color(173, 232, 244));
-      
-      
+
       jtf_correo.setBounds(300, 150, 140, 20);
-      
+
       jpf_pass.setBounds(300, 200, 140, 20);
-      
+
       jb_entrar.setBounds(350, 300, 70, 30);
-      
+
       // ARMADO
       jp_login.add(jtf_correo);
       jp_login.add(jl_correo);
@@ -66,34 +64,34 @@ public class VistaEntrar {
       jp_login.add(jl_titulo);
       jp_login.add(jl_imagen);
       jp_login.add(jb_entrar);
-      
+
       JMenu menuPacientes = WindowMain.menuPacientes;
       JMenu menuUsuarios = WindowMain.menuUsuarios;
       JMenu menuCitas = WindowMain.menuCitas;
       JMenu menuOtros = WindowMain.menuOtros;
-      
+
       JMenuItem itemConsultar = WindowMain.itemConsultar;
       JMenuItem itemEliminar = WindowMain.itemEliminar;
       JMenuItem itemModificar = WindowMain.itemModificar;
       JMenuItem itemAgregar = WindowMain.itemAgregar;
-      
+
       JMenuItem itemEliminar2 = WindowMain.itemEliminar2;
       JMenuItem itemConsultar2 = WindowMain.itemConsultar2;
       JMenuItem itemModificar2 = WindowMain.itemModificar2;
       JMenuItem itemAgregar2 = WindowMain.itemAgregar2;
-      
+
       JMenuItem itemAgendar3 = WindowMain.itemAgendar3;
       JMenuItem itemCancelar3 = WindowMain.itemCancelar3;
       JMenuItem itemModificar3 = WindowMain.itemModificar3;
       JMenuItem itemConsultar3 = WindowMain.itemConsultar3;
       JMenuItem itemRecordatorios3 = WindowMain.itemRecordatorios3;
-      
+
       JMenuItem impresiones = WindowMain.impresiones;
       JMenuItem Salir = WindowMain.Salir;
-      
+
       JMenuBar menuBar = WindowMain.menuBar;
       JFrame jf_ventanaPrincipal = WindowMain.jf_ventanaPrincipal;
-      
+
       // ESCUCHAS
       jb_entrar.addActionListener(new ActionListener() {
         @Override
@@ -102,26 +100,26 @@ public class VistaEntrar {
           menuPacientes.add(itemConsultar);
           menuPacientes.add(itemModificar);
           menuPacientes.add(itemAgregar);
-          
+
           menuUsuarios.add(itemAgregar2);
           menuUsuarios.add(itemEliminar2);
           menuUsuarios.add(itemModificar2);
           menuUsuarios.add(itemConsultar2);
-          
+
           menuCitas.add(itemAgendar3);
           menuCitas.add(itemCancelar3);
           menuCitas.add(itemModificar3);
           menuCitas.add(itemConsultar3);
           menuCitas.add(itemRecordatorios3);
-          
+
           menuOtros.add(impresiones);
           menuOtros.add(Salir);
-          
+
           menuBar.add(menuPacientes);
           menuBar.add(menuUsuarios);
           menuBar.add(menuCitas);
           menuBar.add(menuOtros);
-          
+
           jf_ventanaPrincipal.setJMenuBar(menuBar);
           jf_ventanaPrincipal.add(jp_panelprincipal);
           jf_ventanaPrincipal.remove(jp_login);
@@ -134,5 +132,5 @@ public class VistaEntrar {
     }
     return jp_login;
   }
-  
+
 }
