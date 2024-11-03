4230861c0613b42f05aa13412c991b448ebb8d72
Charls981 <Cuellar.11498@hotmail.com>
2024-11-02 22:50:15 -0600

    Cree un Jpanel donde se agregara el calendario

--- src/view/Citas/VistaCitas.java
+++ src/view/Citas/VistaCitas.java
@@ -1,12 +1,14 @@
 package view.Citas;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.time.LocalDate;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTable;
@@ -16,8 +18,9 @@ import javax.swing.SpinnerModel;
 
 public class VistaCitas {
     private JPanel jp_vistaPrincipalCitas;
-    private JPanel jp_vistaHorarios;
     private JPanel jp_vistaCalendario;
+    private JPanel jp_vistaFecha;
+    private JPanel jp_vistaHorarios;
     private JPanel jp_vistaRecordatorios;
     private JPanel jp_VistaAgregarCliente;
     private LocalDate fechaActual = LocalDate.now();
@@ -25,7 +28,7 @@ public class VistaCitas {
 
     public VistaCitas() {
         JFrame ventana = new JFrame();
-        ventana.add(vistaCalendarioModificado(fechaActual.getYear(),7));
+        ventana.add(VistaFecha());
         ventana.setSize(800, 600);
         ventana.setLocationRelativeTo(null);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -77,22 +80,36 @@ public class VistaCitas {
         return jp_vistaPrincipalCitas;
     }
 
-    // Vista Horarios
-    public JPanel VistaHorarios() {
+    // Vista Fecha
+    public JPanel VistaFecha() {
         // Variables
-
-        // Atributos
+        jp_vistaFecha = new JPanel();
+        JButton jb_volver = new JButton("Volver");
+        JButton jb_Seleccionar = new JButton("Seleccionar");
+        JButton jb_Podologo = new JButton("Podologo");
+        JLabel jl_año_mes = new JLabel();
         // Armado
+        jp_vistaFecha.add(vistaCalendario(fechaInicialdelMes));
+        jp_vistaFecha.add(jl_año_mes);
+        // Atributos
+        jp_vistaFecha.setSize(800, 600);
+        jp_vistaCalendario.setLocation(150, 150);
+        jp_vistaFecha.setBackground(new Color(173, 232, 244));
+        jp_vistaFecha.setLayout(null);
+        jl_año_mes.setBounds(485, 100, 200, 40);
+        jl_año_mes.setText("Año: " + fechaInicialdelMes.getYear() +
+                "  Mes: " + fechaInicialdelMes.getMonth().getValue());
+        jl_año_mes.setFont(new Font("Carlos", Font.ITALIC, 18));
         // Escuchas
-        return jp_vistaHorarios;
+        return jp_vistaFecha;
     }
 
-    private JPanel vistaCalendarioModificado(int año,int mes) {
+    private JPanel vistaCalendarioModificado(int año, int mes) {
         fechaInicialdelMes = LocalDate.of(año, mes, 1);
-        return VistaCalendario(fechaInicialdelMes);
+        return vistaCalendario(fechaInicialdelMes);
     }
 
-    private JPanel VistaCalendario(LocalDate FechaCalendario) {
+    private JPanel vistaCalendario(LocalDate FechaCalendario) {
 
         // if (jp_vistaCalendario == null) {
         // Variables
@@ -107,7 +124,7 @@ public class VistaCitas {
 
         int diaMes = FechaCalendario.getDayOfMonth();
         // Atributos
-        jp_vistaCalendario.setBounds(0, 0, 400, 200);
+        jp_vistaCalendario.setSize(500, 350);
         jp_vistaCalendario.setBackground(new Color(173, 232, 244));
         jp_vistaCalendario.setLayout(estructura);
 
@@ -120,10 +137,13 @@ public class VistaCitas {
                     jl_diasSemanaColumnas[dia] = new JLabel();
                     jl_diasSemanaColumnas[dia].setText(diasSemanaTexto[dia]);
                     jp_vistaCalendario.add(jl_diasSemanaColumnas[dia]);
+                    jp_vistaCalendario.setBackground(new Color(238, 238, 238));
                 }
                 // Se agregan los botones
                 if (semana > 0) {
                     jb_DiasMes[semana - 1][dia] = new JButton();
+                    jb_DiasMes[semana - 1][dia].setBackground(Color.WHITE);
+                    System.out.println(jb_DiasMes[semana - 1][dia].getBackground());
                     jp_vistaCalendario.add(jb_DiasMes[semana - 1][dia]);
                 }
             }
@@ -147,6 +167,15 @@ public class VistaCitas {
         // Escuchas
         // }
         return jp_vistaCalendario;
+    }
+
+    // Vista Horario
+    public JPanel VistaHorario() {
+        // Variables
+        // Atributos
+        // Armado
+        // Escuchas
+        return jp_vistaHorarios;
     }
 
     // Vista Recordatorios
